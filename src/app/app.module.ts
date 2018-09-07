409e7855d5f3e109e2c8a6f8dedeabc55eff835c
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-09-07 12:16:20 -0300

    avanzando en el login, comenzando con componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -9,8 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBlvbLO4JtPw4UT42z2uBSCot9idpl1xsk',
+    authDomain: 'redsocial-3c001.firebaseapp.com',
+    databaseURL: 'https://redsocial-3c001.firebaseio.com',
+    projectId: 'redsocial-3c001',
+    storageBucket: 'redsocial-3c001.appspot.com',
+    messagingSenderId: '199350651583'
+  }
 };
 
 /*
