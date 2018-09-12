dd2859d4d04411a34f41d5cc0abc991357f3c37f
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-09-12 12:41:29 -0300

    avanzando login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AuthService } from './auth.service';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { BotonMenuTComponent } from './boton-menu-t/boton-menu-t.component';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MuroComponent } from './muro/muro.component';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ListaCategoriasComponent } from './lista-categorias/lista-categorias.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +24,13 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
     ToolbarComponent,
     SidenavComponent,
     BotonMenuTComponent,
+    MuroComponent,
+    ListaCategoriasComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp (environment.firebase),
     AngularFireAuthModule,
     LayoutModule,
     MatToolbarModule,
@@ -33,6 +39,8 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
     MatIconModule,
     MatListModule,
     MatButtonToggleModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private firebaseAuth: AngularFireAuth) {
+    this.user = firebaseAuth.authState;
+  }
+  signup(email: string, password: string) {
+    this.firebaseAuth
+      .auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Se ha registrado con éxito', value);
+      })
+      .catch(err => {
+        console.log('Falló el registro', err.message);
+      });
+  }
+
+  login(email: string, password: string) {
+    this.firebaseAuth
+      .auth
+      .signInWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Lgin exitoso');
+      })
+      .catch(err => {
+        console.log('No se pudo log-in', err.message);
+      });
+  }
+
+  logout() {
+    this.firebaseAuth
+      .auth
+      .signOut();
+  }
 }
